511ec8d0da358596dd5c5063fc33f5b92441e2aa
Mark <mynameismarknoob@gmail.com>
2025-05-18 19:59:43 +0700

    Add Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\MenusController;
 use App\Http\Controllers\MenusDeckController;
 use App\Http\Controllers\MenusDeckExpensesController;
@@ -7,35 +11,53 @@ use App\Http\Controllers\MenusDeckPaymentsController;
 use App\Http\Controllers\MenusRecomenderController;
 use App\Http\Controllers\VendorsController;
 use App\Models\Menu;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard.index');
 });
 
-Route::get('/db', function () {
-    return view('dbcon');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/db-data', function () {
-    return view('db-data', ['title' => 'DB-Data', 'menus' => Menu::all(), 'targetMenu' => Menu::find(1)]);
-});
+
 
 // Master
-Route::resource('menus', MenusController::class);
-Route::resource('vendors', VendorsController::class);
+Route::resource('menus', MenusController::class)->middleware(['auth', 'verified']);
+Route::resource('vendors', VendorsController::class)->middleware(['auth', 'verified']);
+
 
 
 // Transaksi
-Route::resource('menus-deck', MenusDeckController::class);
-Route::resource('menus-recommender', MenusRecomenderController::class);
-Route::resource('menus-deck-expenses', MenusDeckExpensesController::class);
-Route::resource('menus-deck-payments', MenusDeckPaymentsController::class);
+Route::resource('menus-deck', MenusDeckController::class)->middleware(['auth', 'verified']);
+Route::resource('menus-recommender', MenusRecomenderController::class)->middleware(['auth', 'verified']);
+Route::resource('menus-deck-expenses', MenusDeckExpensesController::class)->middleware(['auth', 'verified']);
+Route::resource('menus-deck-payments', MenusDeckPaymentsController::class)->middleware(['auth', 'verified']);
 
-Route::post('/menus-recommender/calculate-similarity', [MenusRecomenderController::class, 'calculateSimilarity'])->name('menus-recommender.similarity');
-Route::get('menus-deck/create/{menu_id?}', [MenusDeckController::class, 'create'])->name('menus-deck.create');
-Route::patch('/menus-deck-expenses/{id}/delete', [MenusDeckExpensesController::class, 'delete'])->name('menus-deck-expenses.delete');
-Route::post('/menus-recommender/ge', [MenusRecomenderController::class, 'calculateSimilarity'])->name('menus-recommender.similarity');
-
+// Route::post('/menus-recommender/calculate-similarity', [MenusRecomenderController::class, 'calculateSimilarity'])->name('menus-recommender.similarity')->middleware(['auth', 'verified']);
+// Route::get('menus-deck/create/{menu_id?}', [MenusDeckController::class, 'create'])->name('menus-deck.create')->middleware(['auth', 'verified']);
+// Route::patch('/menus-deck-expenses/{id}/delete', [MenusDeckExpensesController::class, 'delete'])->name('menus-deck-expenses.delete')->middleware(['auth', 'verified']);
 
 
+
+// Dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard')
+    ->middleware(['auth', 'verified']);
+Route::get('/dashboard/pengeluaran', [DashboardController::class, 'pengeluaran'])
+    ->name('dashboard.pengeluaran')
+    ->middleware(['auth', 'verified']);
+Route::get('/dashboard/pembayaran', [DashboardController::class, 'pembayaran'])
+    ->name('dashboard.pembayaran')
+    ->middleware(['auth', 'verified']);
+
+
+
+
+require __DIR__.'/auth.php';
